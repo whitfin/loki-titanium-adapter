773c9ee574f8c605dfcc3b96cd60f3e873ea0672
Isaac Whitfield <iw@whitfin.io>
2019-05-30 17:28:13 -0700

    Add initial implementation of adapter

---
+++ README.md
@@ -0,0 +1,57 @@
+# loki-titanium-adapter
+[![Build Status](https://img.shields.io/travis/whitfin/loki-titanium-adapter.svg)](https://travis-ci.org/whitfin/loki-titanium-adapter)
+![npm](https://img.shields.io/npm/v/loki-titanium-adapter.svg)
+
+Titanium SDK adapter for the [Loki](https://github.com/techfort/LokiJS)
+embedded database.
+
+You can use this library to persist Loki databases inside Titanium applications
+using the Titanium API. Everything else operates as it would in any other runtime,
+so visit the [Loki documentation](https://github.com/techfort/LokiJS) for further
+information.
+
+### Usage
+
+This module is on npm, so feel free to grab from there (as well as Loki):
+
+```shell
+$ npm i lokijs loki-titanium-adapter
+```
+
+You can then configure it inside your application pretty easily, as the
+API is still synchronous for the time being:
+
+```javascript
+// Load our modules
+const Loki = require('lokijs');
+const TitaniumAdapter = require('loki-titanium-adapter');
+
+// Construct our database instance
+const db = new Loki('my-database', {
+	adapter: new TitaniumAdapter(),
+	autoload: true,
+	autosave: true,
+	autosaveInterval: 5000,
+	autoloadCallback: function () {
+    // called when your database is loaded
+	},
+});
+```
+
+This will save your database changes to disk every 5 seconds (configured via
+the `autosaveInterval` parameter). In addition to this, I recommend adding a
+hook for the `pause` event to flush when the application is closed:
+
+```javascript
+// Add a pause listener to ensure flush on background
+Ti.App.addEventListener('pause', function (_e) {
+	db.saveDatabase(function (e) {
+    // documents should have been flushed
+	});
+});
+```
+
+There is also a more complete [example app](example/) available - just keep
+in mind that you need to run `npm install` before you try to run it. For any
+further use, check out [Loki wiki](https://github.com/techfort/LokiJS/wiki)
+as the API is exactly the same.
---
+++ example/Resources/app.js
@@ -0,0 +1,142 @@
+// Load our PouchDB module dependencies
+const Loki = require('lokijs');
+const TitaniumAdapter = require('loki-titanium-adapter');
+
+// Construct our database instance
+const db = new Loki('values', {
+	adapter: new TitaniumAdapter(),
+	autoload: true,
+	autosave: false,
+	autosaveInterval: 5000,
+	autoloadCallback: function () {
+		if (!db.getCollection('values')) {
+			db.addCollection('values');
+		}
+		_reset();
+	},
+});
+
+// Create our window instance
+let win = Ti.UI.createWindow({
+	backgroundColor: 'white',
+	layout: 'vertical'
+});
+
+// Create our text submit button
+let submit = Ti.UI.createButton({
+	title: 'Insert',
+	height: 24,
+	width: 75
+});
+
+// Create our insertion text field
+let insert = Ti.UI.createTextField({
+	top: 50,
+	height: 35,
+	width: 200,
+	backgroundColor: 'white',
+	hintText: 'Enter a value',
+	borderStyle: Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+	keyboardType: Titanium.UI.KEYBOARD_DEFAULT,
+	rightButton: submit
+});
+
+// Create our list of values to display
+let records = Ti.UI.createListView({
+	defaultItemTemplate: 'default',
+	headerTitle: 'Values',
+	sections: [],
+	templates: {
+		default: {
+			childTemplates: [
+				{
+					type: 'Ti.UI.Label',
+					bindId: 'value',
+					properties: {
+						left: 20,
+						height: 44
+					}
+				},
+				{
+					type: 'Ti.UI.Button',
+					bindId: 'remove',
+					properties: {
+						width: 75,
+						height: 24,
+						right: 10,
+						title: 'Remove'
+					},
+					events: {
+						click: function report(e) {
+							db.getCollection('values').remove(e.itemId);
+							_reset();
+						}
+					}
+				}
+			]
+		}
+	}
+});
+
+// Add a listener to insert the provided value to the database.
+//
+// This will then update the table with the latest values from the
+// database. This persists across restarts, meaning that if you kill
+// the app and re-open it, the state will still be available and the
+// last known values will be shown.
+submit.addEventListener('click', function () {
+	if (insert.value.length === 0) {
+		return;
+	}
+	db.getCollection('values').insert({
+		value: insert.value
+	});
+	_reset();
+});
+
+// Unfocus the text field when clicked elsewhere
+win.addEventListener('click', function (_e) {
+	insert.blur();
+});
+
+// Add a pause listener to ensure flush on background
+Ti.App.addEventListener('pause', function (_e) {
+	db.saveDatabase(function () {
+		Ti.API.info('Flushing database changes');
+	});
+});
+
+// Add all window components
+win.add(insert);
+win.add(records);
+win.open();
+
+// Reset handler to re-populate the list view.
+//
+// This will fetch the values from the table and display them in the
+// list. This recreates the list from scratch as this is just for
+// demonstration purposes, not necessarily for best efficiency.
+function _reset() {
+	// reset the insert text
+	insert.value = '';
+
+	// fetch all values from the database
+	let entries = db.getCollection('values').find();
+
+	// create a section from each entry in the database
+	let section = Ti.UI.createListSection({
+		items: entries.map(function (record) {
+			return {
+				value: {
+					text: record.value
+				},
+				properties: {
+					itemId: record.$loki
+				}
+			};
+		})
+	});
+
+	// update the list with the new section
+	records.sections = [section];
+}
